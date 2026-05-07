76bd7b764907eaf760bddd4fbee627f9c24f89dc
tardis <tardis@tardisui-MacBookPro.local>
2026-05-07 12:18:32 +0900

    feat: add SkillKeyword model, repository, and JobService keyword/enrich methods

--- db/models/__init__.py
+++ db/models/__init__.py
@@ -5,5 +5,6 @@ from db.models.job_detail import JobDetail
 from db.models.search_preset import SearchPreset
 from db.models.job_skip import JobSkip
 from db.models.job_evaluation import JobEvaluation
+from db.models.skill_keyword import SkillKeyword
 
-__all__ = ["Base", "Job", "Application", "JobDetail", "SearchPreset", "JobSkip", "JobEvaluation"]
+__all__ = ["Base", "Job", "Application", "JobDetail", "SearchPreset", "JobSkip", "JobEvaluation", "SkillKeyword"]
